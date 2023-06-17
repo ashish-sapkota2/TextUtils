6267006ad0d9b4c14200f9bde69c0f4857cf2d44
Ashish Sapkota <aryanashish223@gmail.com>
2023-06-17 21:43:10 +0545

    textutils- react learning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+import React ,{useState} from 'react';
+// import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 function App() {
+  const [mode, setmode] = useState("light"); // whether dark mode is enabled or not
+  const [text, setText] = useState("Enable dark Mode");
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) =>{
+    setAlert ({
+      msg: message,
+      type : type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
+  let handleGreen=()=>{
+    if(mode==='light'){
+      setmode("green");
+      document.body.style.backgroundColor= 'green';
+    }else{
+      setmode('light');
+      document.body.style.background= 'white';
+      document.body.style.backgroundColor= 'white';
+    }
+  }
+  let toggleMode =()=>{
+    if(mode==='light'|| mode ==='green'){
+      setmode('dark');
+      document.body.style.backgroundColor='grey';
+      setText("Enable Light Mode");
+      showAlert("Dark mode has been enabled ", "success");
+      document.title ='TExtUtil-Dark Mode';
+
+    }else{
+      setmode('light');
+      document.body.style.backgroundColor='white';
+      setText("Enable Dark Mode");
+      showAlert("Light mode has been enabled ", "success");
+      document.title ='TExtUtil-Light Mode';
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar title="TextUtils" about="About Us" mode = {mode} toggleMode={toggleMode}  handleGreen ={handleGreen} text ={text}/>
+      {/* <Navbar title="TextUtils" about="About Us" mode = {mode} handleGreen ={handleGreen} text ={text}/> */}
+      <Alert alert = {alert}/>
+     <div className="container my-3">   
+        <Routes>
+          <Route exact path="/about" element={<About  mode ={mode} />}>
+            {/* <About /> */}
+          </Route>
+          <Route exact path="/" element={<TextForm showAlert ={showAlert} heading ="Enter the text here" mode ={mode}/>}>
+            
+          </Route>
+        </Routes>   
+      </div>
+    </Router>
+
+     {/* <About/> */}
+
+    </>
   );
 }
-
 export default App;
+
---
+++ src/components/TextForm.js
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+
+export default function (props) {
+    const handleUpClick =()=>{
+        // console.log("button clicked" +text);
+        let newText = text.toUpperCase();
+        setText(newText);
+        props.showAlert("Converted to uppper case", "success");
+    }
+    const handleClick =()=>{
+        // console.log("button clicked" +text);
+        let newText = text.split(" ").reverse().join(" ");
+        setText(newText);
+    }
+    const handleLowClick =()=>{
+      // console.log("button clicked" +text);
+      let newText = text.toLowerCase();
+      setText(newText);
+      props.showAlert("Converted to Lower case", "success");
+  };
+    const handleOnChange =(event)=>{
+        // console.log("changed");
+        const word = event.target.value;
+        setText(word);
+        setWordCount(countWord(word));
+    };
+    const countWord=(string)=>{
+      const text =string.trim().split(" ").length;
+      return text;
+    }
+    const [text, setText] = useState('');
+    const [wordCount, setWordCount] = useState(0);
+    // setText("new Text");
+  return (
+    <>
+    <div>
+        <h2 style ={{color:props.mode ==='light'? 'black':'white'}}>{props.heading}</h2>
+<div className="mb-3">
+  <textarea className="form-control"  value ={text} onChange= {handleOnChange} style ={{backgroundColor:props.mode ==='light'? 'white':'#a49b9b', color:props.mode==='light'? 'black':'white'}} id="myBox" rows="5"></textarea>
+</div>
+<button disabled ={text.length ===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to  uppercase</button>
+<button disabled ={text.length ===0} className="btn btn-primary mx-1 my-1" onClick={handleLowClick}>Convert to  lowercase</button>
+<button disabled ={text.length ===0} className="btn btn-primary mx-1 my-1" onClick={handleClick}>Reverse</button>
+    </div>
+<div className="container my-3" style ={{color:props.mode ==='light'? 'black':'white'}}>
+  <h2 >Your text summary</h2>
+  <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words , {text.length} characters</p>
+  <p>{0.008* text.split(" ").filter((element)=>{return element.length!==0}).length} Minute read</p>
+  <h2>Preview</h2>
+  <p>{text.length>0?text:"Nothing to preview here"}</p>
+</div>
+    </>
+  )
+}
